e019d84fab3daa9fbc8d6d4529da160e1b9ed1c6
Jordan Hunt <jordan.hunt@mendix.com>
2022-05-24 15:04:07 +0200

    feat(): poll log file and cleanup2

--- scripts/test/e2e-native.js
+++ scripts/test/e2e-native.js
@@ -5,6 +5,7 @@ const { cat, mkdir, rm, mv, cp } = require("shelljs");
 const { pipeline } = require("stream");
 const { promisify } = require("util");
 const { createWriteStream } = require("fs");
+const { readFile } = require("fs/promises");
 const { tmpdir } = require("os");
 
 main().catch(e => {
@@ -13,16 +14,18 @@ main().catch(e => {
 });
 
 async function main() {
-    const mendixVersion = await getMendixVersion();
     // todo: there is no FORKED envvar. consider using `external-pull-request`.
+    const mendixVersion = await getMendixVersion();
     const ghcr = process.env.CI && process.env.FORKED !== "true" ? "ghcr.io/mendix/widgets-resources/" : "";
-
     const branch = "main";
-    const testArchivePath = await getTestProject("https://github.com/mendix/Native-Mobile-Resources", branch);
+    const project = "Native-Mobile-Resources";
+    const testArchivePath = await getTestProject(`https://github.com/mendix/${project}`, branch);
     const localRoot = process.cwd();
     const testsDir = join(localRoot, "tests");
     const testProjectDir = join(testsDir, "testProject");
     const repoPath = join(testsDir, `Native-Mobile-Resources-${branch}`);
+
+    console.log(`Working with ${project} on branch ${branch}`);
 
     mkdir("-p", join(localRoot, "tests"));
     execSync(`unzip -o ${testArchivePath} -d ${testsDir}`);
@@ -54,7 +57,7 @@ async function main() {
     // When running on CI pull the docker image from Github Container Registry
     if (ghcr) {
         console.log(`Pulling mxbuild docker image from Github Container Registry...`);
-        execSync(`docker pull ${ghcr}mxbuild:${mendixVersion}`);
+        execSync(`docker pull ${ghcr}mxbuild:${mendixVersion}`, { stdio: "inherit" });
     }
 
     const existingImages = execSync(`docker image ls -q ${ghcr}mxbuild:${mendixVersion}`).toString().trim();
@@ -72,7 +75,7 @@ async function main() {
 
     if (ghcr) {
         console.log(`Pulling mxruntime docker image from Github Container Registry...`);
-        execSync(`docker pull ${ghcr}mxruntime:${mendixVersion}`);
+        execSync(`docker pull ${ghcr}mxruntime:${mendixVersion}`, { stdio: "inherit" });
     }
 
     const existingRuntimeImages = execSync(`docker image ls -q ${ghcr}mxruntime:${mendixVersion}`).toString().trim();
@@ -118,18 +121,57 @@ async function main() {
                 `start --port '8083' --verbose --config '/source/tests/testProject/deployment/native/metro.config.json' > /source/tests/testProject/deployment/log/packager.txt"`
         );
 
-        await tryReach("Metro bundler", () => fetchUrl("http://localhost:8083/status"));
+        await tryReach("Metro bundler", () => fetch("http://localhost:8083/status"));
 
         console.log("Preheating bundler for Android dev=false minify=true");
         const makeItNativeArguments =
             "?platform=android&dev=false&minify=true&app=com.mendix.developerapp.mx9&modulesOnly=false&runModule=true";
-        await tryReach(
-            "Bundler",
-            () => fetchOrTimeout(`http://localhost:8083/index.bundle${makeItNativeArguments}`),
-            30,
-            60 * 3
-        );
-        console.log("Preheating done!");
+
+        fetch(`http://localhost:8083/index.bundle${makeItNativeArguments}`)
+            .then(async response => {
+                const { ok, status, statusText } = response;
+                if (ok) {
+                    console.log("Preheating done!");
+                } else {
+                    console.error(`Metro error ${status}: ${statusText}\n${await response.text()}`);
+                }
+            })
+            .catch(e => {
+                console.error("Request for index.js from Metro failed.");
+                console.error(e);
+            });
+
+        const pauseInS = 60;
+        let attempts = 10000000;
+
+        for (; attempts > 0; --attempts) {
+            try {
+                console.log("Reading Metro packager file...");
+
+                const file = (await readFile(join(testProjectDir, "deployment", "log", "packager.txt"))).toString();
+                if (new RegExp(/BUNDLE[\s]+.\/index.js/).test(file)) {
+                    break;
+                }
+            } catch (e) {
+                console.error(e);
+            }
+            console.log(`Log line not found, pausing ${pauseInS} seconds...`);
+
+            await new Promise(resolve => setTimeout(resolve, pauseInS * 1000));
+        }
+
+        if (attempts === 0) {
+            throw new Error("Did not create Metro bundle ");
+        }
+
+        console.log("Metro bundle created!");
+
+        // await tryReach(
+        //     "Bundler",
+        //     () => fetchOrTimeout(`http://localhost:8083/index.bundle${makeItNativeArguments}`),
+        //     30,
+        //     60 * 3
+        // );
 
         // Spin up the runtime and run the testProject
         runtimeContainerId = execSync(
@@ -140,7 +182,7 @@ async function main() {
             .toString()
             .trim();
 
-        await tryReach("Runtime", () => fetchUrl("http://localhost:8080"));
+        await tryReach("Runtime", () => fetch("http://localhost:8080"));
 
         console.log("Setup for android...");
         execSync("npm run setup-android", { stdio: "inherit" });
@@ -170,6 +212,7 @@ async function main() {
 }
 
 async function getTestProject(repository, branch) {
+    console.log("Downloading test project...");
     const downloadedArchivePath = join(tmpdir(), `testProject.zip`);
 
     if (!repository.includes("github.com")) {
@@ -183,6 +226,8 @@ async function getTestProject(repository, branch) {
             ).body,
             createWriteStream(downloadedArchivePath)
         );
+        console.log("Done!");
+
         return downloadedArchivePath;
     } catch (e) {
         console.log(`Url is not available :(`);
@@ -221,33 +266,23 @@ async function getMendixVersion() {
     return mendixVersion;
 }
 
-async function fetchUrl(url) {
-    return (await fetch(url)).ok;
-}
-
-async function fetchOrTimeout(url) {
-    return await Promise.race([
-        fetch(url).then(response => {
-            if (!response.ok) {
-                throw new HTTPResponseError(response, "from Metro");
-            }
-            return true;
-        }),
-        new Promise((_, reject) => setTimeout(() => reject(new Error("Preheating call timed out!")), 10 * 60 * 1000))
-    ]);
-}
-
-async function tryReach(name, fn, attempts = 60, pause = 3) {
+async function tryReach(name, request, attempts = 60, pause = 3) {
     for (; attempts > 0; --attempts) {
         try {
             console.log(`Trying to reach ${name}...`);
-            if (await fn()) {
+
+            const { ok, status, statusText } = await request();
+            if (ok) {
                 break;
+            } else {
+                // the request worked, but is not OK status. try again.
+                console.error(`Request failed, status: ${status}: ${statusText}`);
             }
         } catch (e) {
             console.error(e);
-            console.log(`Could not reach ${name}, pausing...`);
         }
+        console.log(`Could not reach ${name}, pausing...`);
+
         await new Promise(resolve => setTimeout(resolve, pause * 1000));
     }
     if (attempts === 0) {
@@ -255,11 +290,4 @@ async function tryReach(name, fn, attempts = 60, pause = 3) {
     }
 
     console.log(`${name} is up!`);
-}
-
-class HTTPResponseError extends Error {
-    constructor(response, ...args) {
-        super(`HTTP Response Error: ${response.status} ${response.statusText} ${args.join(" ")}`);
-        this.response = response;
-    }
 }
