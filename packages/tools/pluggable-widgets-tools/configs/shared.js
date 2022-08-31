192c76512bc7c1e6cf9cb72da44b529b4e09c81e
宋正旭 <91315@runjian.com>
2022-08-31 09:41:17 +0800

    feat: handle multiple file entries

--- packages/tools/pluggable-widgets-tools/configs/shared.js
+++ packages/tools/pluggable-widgets-tools/configs/shared.js
@@ -19,25 +19,52 @@ export const sourcePath = process.cwd();
 const widgetPackageJson = require(join(sourcePath, "package.json"));
 export const widgetName = widgetPackageJson.widgetName;
 export const widgetPackage = widgetPackageJson.packagePath;
+export const widgetPath = widgetPackageJson.widgetPath;
 export const widgetVersion = widgetPackageJson.version;
 if (!widgetName || !widgetPackageJson) {
     throw new Error("Widget does not define widgetName in its package.json");
 }
 
 const widgetSrcFiles = readdirSync(join(sourcePath, "src")).map(file => join(sourcePath, "src", file));
-export const widgetEntry = widgetSrcFiles.filter(file =>
-    file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.[jt]sx?$`, "i"))
-)[0];
-if (!widgetEntry) {
+
+let widgetEntry = [
+    widgetSrcFiles.filter(file => file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.[jt]sx?$`, "i")))[0]
+];
+if (!widgetEntry.length === 0) {
     throw new Error("Cannot find a widget entry file");
 }
 
-export const editorConfigEntry = widgetSrcFiles.filter(file =>
-    file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.editorConfig\\.[jt]s$`, "i"))
-)[0];
-export const previewEntry = widgetSrcFiles.filter(file =>
-    file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.(webmodeler|editorPreview)\\.[jt]sx?$`, "i"))
-)[0];
+let editorConfigEntry = [
+    widgetSrcFiles.filter(file =>
+        file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.editorConfig\\.[jt]s$`, "i"))
+    )[0]
+];
+let previewEntry = [
+    widgetSrcFiles.filter(file =>
+        file.match(new RegExp(`[/\\\\]${escape(widgetName)}\\.(webmodeler|editorPreview)\\.[jt]sx?$`, "i"))
+    )[0]
+];
+
+// Handle multiple file entries
+if (widgetPath && widgetPath.length > 0) {
+    widgetEntry = widgetPath.map(
+        item => widgetSrcFiles.filter(file => file.match(new RegExp(`[/\\\\]${escape(item)}\\.[jt]sx?$`, "i")))[0]
+    );
+    editorConfigEntry = widgetPath.map(
+        item =>
+            widgetSrcFiles.filter(file =>
+                file.match(new RegExp(`[/\\\\]${escape(item)}\\.editorConfig\\.[jt]s$`, "i"))
+            )[0]
+    );
+    previewEntry = widgetPath.map(
+        item =>
+            widgetSrcFiles.filter(file =>
+                file.match(new RegExp(`[/\\\\]${escape(item)}\\.(webmodeler|editorPreview)\\.[jt]sx?$`, "i"))
+            )[0]
+    );
+}
+
+export { widgetEntry, editorConfigEntry, previewEntry };
 
 export const isTypescript = [widgetEntry, editorConfigEntry, previewEntry].some(file => file && /\.tsx?$/.test(file));
 
